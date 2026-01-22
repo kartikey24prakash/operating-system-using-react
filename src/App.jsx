42ff6664052fcd623b954054fae530765ba42218
kartikey <kp@gamil.com>
2026-01-23 03:14:12 +0530

    build the dragable window

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import "./app.scss"
 import Dock from './components/Dock.jsx'
 import Nav from './components/Nav.jsx'
+import MacosWindow from './components/Windows/MacosWindow.jsx'
 
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
    <main>
     <Nav/>
     <Dock/>
-
-
+    <MacosWindow>
+      <h1>Hello, World!</h1>
+    </MacosWindow>
    </main>
   )
 }
---
+++ src/components/Windows/MacosWindow.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Rnd } from 'react-rnd'
+import "./window.scss"
+
+const MacosWindow = ({children}) => {
+  return (
+  <Rnd>
+
+
+    <div className="window">
+        <div className="nav">
+            <div className="dots">
+                <div className="dot red"></div>
+                <div className="dot yellow"></div>
+                <div className="dot green"></div>
+            </div>
+            <div className="title"><p>kartikeyprkash - zsh</p></div>
+        </div>
+        <div className="main-content">
+            {children}
+        </div>
+    </div>  
+
+    </Rnd>
+    
+  )
+}
+
+export default MacosWindow
